ab7d8b46527e6b7ced977638bb6a0a5f396cb0b5
Raman Prabhakar <raman.prabhakar@zestmoney.in>
2020-12-31 17:15:16 +0530

    add employee model

---
+++ src/models/employee.model.ts
@@ -0,0 +1,40 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Employee extends Entity {
+  @property({
+    type: 'number',
+    id: true,
+    generated: true,
+  })
+  id?: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  raman: string;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  experience: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  domain: string;
+
+
+  constructor(data?: Partial<Employee>) {
+    super(data);
+  }
+}
+
+export interface EmployeeRelations {
+  // describe navigational properties here
+}
+
+export type EmployeeWithRelations = Employee & EmployeeRelations;
